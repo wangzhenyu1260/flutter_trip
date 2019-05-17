c86b63218b77f7461d0a07c04bc22a696de9864a
zhenyu <wangzhenyu1260@126.com>
2019-05-17 18:05:06 +0800

    Push：9-8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_trip/dao/home_dao.dart';
+import 'package:flutter_trip/model/common_model.dart';
+import 'package:flutter_trip/model/home_model.dart';
+import 'dart:convert';
+
+import 'package:flutter_trip/widget/grid_nav.dart';
+import 'package:flutter_trip/widget/local_nav.dart';
+
 const APPBAR_SCROLL_OFFSET = 100;
 
 class HomePage extends StatefulWidget {
@@ -14,7 +22,15 @@ class _HomePageState extends State<HomePage> {
     'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
   ];
   double appBarAlpha = 0;
-  _onScroll(offset){
+  List<CommonModel> localNavList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  _onScroll(offset) {
     double alpha = offset / APPBAR_SCROLL_OFFSET;
     if (alpha < 0) {
       alpha = 0;
@@ -27,62 +43,88 @@ class _HomePageState extends State<HomePage> {
     print(appBarAlpha);
   }
 
+  loadData() async {
+//    HomeDao.fetch().then((result) {
+//      setState(() {
+//        resultString = json.encode(result);
+//      });
+//    }).catchError((e){
+//      setState(() {
+//        resultString = e.toString();
+//      });
+//    });
+    try {
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        localNavList = model.localNavList;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            MediaQuery.removePadding(
-                removeTop: true,
-                context: context,
-                child: NotificationListener(
-                  onNotification: (scrollNotification){
-                    if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0){
-                      //滚动且是列表滚动的时候
-                      _onScroll(scrollNotification.metrics.pixels);
-                    }
-                  },
-                  child: ListView(
-                    children: <Widget>[
-                      Container(
-                        height: 160,
-                        child: Swiper(
-                          itemCount: _imageUrls.length,
-                          autoplay: true,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Image.network(
-                              _imageUrls[index],
-                              fit: BoxFit.fill,
-                            );
-                          },
-                          pagination: SwiperPagination(),
-                        ),
-                      ),
-                      Container(
-                        height: 800,
-                        child: ListTile(
-                          title: Text('哈哈'),
-                        ),
-                      )
-                    ],
+      backgroundColor: Color(0xfff2f2f2),
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  //滚动且是列表滚动的时候
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
                   ),
-                )
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                    child: LocalNav(localNavList: localNavList),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('resultString'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Opacity(
-              opacity: appBarAlpha,
-              child: Container(
-                height: 80,
-                decoration: BoxDecoration(color: Colors.white),
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Text('首页'),
-                  ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
                 ),
               ),
-            )
-          ],
-        )
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widget/webview.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebView extends StatefulWidget {
+  final String url;
+  final String statusBarColor;
+  final String title;
+  final bool hideAppBar;
+  final bool backForbid;
+
+  WebView(
+      {this.url,
+      this.statusBarColor,
+      this.title,
+      this.hideAppBar,
+      this.backForbid});
+
+  @override
+  State<StatefulWidget> createState() {
+    _WebViewState createState() => _WebViewState();
+    return null;
+  }
+}
+
+class _WebViewState extends State<WebView> {
+  final webviewReference = FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
+  StreamSubscription<WebViewHttpError> _onHttpError;
+
+  @override
+  void initState() {
+    super.initState();
+    webviewReference.close();
+    _onUrlChanged = webviewReference.onUrlChanged.listen((String url) {});
+    _onStateChanged = webviewReference.onStateChanged.listen((WebViewStateChanged state) {
+      switch (state.type) {
+        case WebViewState.startLoad:
+          break;
+        default:
+          break;
+      }
+    });
+    _onHttpError = webviewReference.onHttpError.listen((WebViewHttpError error) {
+      print(error);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _onUrlChanged.cancel();
+    _onStateChanged.cancel();
+    _onHttpError.cancel();
+    webviewReference.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
