a90822355b458ff3e1adec9ae107daf1a70aa503
zhenyu <wangzhenyu1260@126.com>
2019-07-05 16:51:22 +0800

    Push：android语音搜索

--- android/app/src/main/java/org/devio/flutter/flutter_trip/MainActivity.java
+++ android/app/src/main/java/org/devio/flutter/flutter_trip/MainActivity.java
@@ -1,6 +1,9 @@
 package org.devio.flutter.flutter_trip;
 
 import android.os.Bundle;
+
+import org.devio.flutter.plugin.asr.AsrPlugin;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +12,10 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    registerSelfPlugin();
+  }
+
+  private void registerSelfPlugin() {
+    AsrPlugin.registerWith(registrarFor("org.devio.flutter.plugin.asr.AsrPlugin"));
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_trip/dao/search_dao.dart';
 import 'package:flutter_trip/model/search_model.dart';
+import 'package:flutter_trip/pages/speak_page.dart';
 import 'package:flutter_trip/widget/search_bar.dart';
 import 'package:flutter_trip/widget/webview.dart';
 
@@ -39,6 +40,14 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   SearchModel searchModel;
   String keyword;
+
+  @override
+  void initState() {
+    if(widget.keyword != null) {
+      _onTextChange(widget.keyword);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -103,6 +112,7 @@ class _SearchPageState extends State<SearchPage> {
               hideLeft: widget.hideLeft,
               defaultText: widget.keyword,
               hint: widget.hint,
+              speakClick: _jumpToSpeak,
               leftButtonClick: () {
                 Navigator.pop(context);
               },
@@ -111,6 +121,15 @@ class _SearchPageState extends State<SearchPage> {
           ),
         ),
       ],
+    );
+  }
+
+  _jumpToSpeak() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SpeakPage(),
+      ),
     );
   }
 
